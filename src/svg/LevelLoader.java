c2b99c593b49a2804ea542d7e44f4373ed769d1b
Chad Price <chad@chad-laptop.(none)>
2014-01-18 15:03:51 -0800

    Added loading player position and dimensions from level files.

--- src/svg/LevelLoader.java
+++ src/svg/LevelLoader.java
@@ -18,41 +18,51 @@ public class LevelLoader {
 			DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
 			Document doc = docBuilder.parse(new File(filename));
-			
+
 			doc.getDocumentElement().normalize();
-			
+
 			Level level = new Level();
-			
+
 			NodeList listOfRectangles = doc.getElementsByTagName("rect");
 			// NodeList is not iterable, so we have to loop manually.
 			for (int i = 0; i < listOfRectangles.getLength(); i++) {
 				Element rectangle = (Element) listOfRectangles.item(i);
-				
+
 				float width = Float.parseFloat(rectangle.getAttribute("width"));
 				float height = Float.parseFloat(rectangle.getAttribute("height"));
 				Vec2 dim = new Vec2(width, height);
 				float x = Float.parseFloat(rectangle.getAttribute("x"));
 				float y = Float.parseFloat(rectangle.getAttribute("y"));
 				Vec2 pos = new Vec2(x, y);
-				
-				level.addPlatform(new Platform(pos, dim));
-				
-				System.out.println("Platform:");
-				System.out.println("\twidth: " + width);
-				System.out.println("\theight: " + height);
-				System.out.println("\tx: " + x);
-				System.out.println("\ty: " + y);
+
+				String style = rectangle.getAttribute("style");
+				if (style.equals("fill:#ff00ff")) {
+					level.setPlayerInitialPosition(pos);
+					level.setPlayerInitialDimensions(dim);
+					System.out.println("Player:");
+					System.out.println("\twidth: " + width);
+					System.out.println("\theight: " + height);
+					System.out.println("\tx: " + x);
+					System.out.println("\ty: " + y);
+				} else {
+					level.addPlatform(new Platform(pos, dim));
+					System.out.println("Platform:");
+					System.out.println("\twidth: " + width);
+					System.out.println("\theight: " + height);
+					System.out.println("\tx: " + x);
+					System.out.println("\ty: " + y);
+				}
 			}
-			
+
 			return level;
 		} catch (ParserConfigurationException e) {
 			e.printStackTrace();
 		} catch (SAXException e) {
 			e.printStackTrace();
 		}
-		
+
 		// This only happens if there is a parsing error.
 		return null;
 	}
-	
+
 }
