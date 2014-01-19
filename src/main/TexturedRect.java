0b9e352b9c892aa72da3f71dc3cb7fa3ed5f262b
Dylan Swiggett <dylanswiggett@gmail.com>
2014-01-18 23:31:22 -0800

    Fixes stuff with textures and stuff.

--- src/main/TexturedRect.java
+++ src/main/TexturedRect.java
@@ -64,14 +64,17 @@ public class TexturedRect implements Drawable{
 		
 		GL11.glBegin(GL11.GL_QUADS);
 		{
-			GL11.glVertex2f(0, 1);
-			GL11.glTexCoord2f(0, 1);
-			GL11.glVertex2f(0, 0);
-			GL11.glTexCoord2f(1, 1);
-			GL11.glVertex2f(1, 0);
-			GL11.glTexCoord2f(1, 0);
-			GL11.glVertex2f(1, 1);
 			GL11.glTexCoord2f(0, 0);
+			GL11.glVertex2f(0, 1);
+			GL11.glTexCoord2f(0, texture.getHeight());
+			GL11.glVertex2f(0, 0);
+			GL11.glTexCoord2f(texture.getWidth(), texture.getHeight());
+			
+			GL11.glVertex2f(1, 0);
+			GL11.glTexCoord2f(texture.getWidth(), 0);
+			
+			GL11.glVertex2f(1, 1);
+			
 		}
 		GL11.glEnd();
 		
