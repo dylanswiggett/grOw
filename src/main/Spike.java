76c9a54dcef86eb9c33ef9314f5b1ca2dc0115c5
Chad Price <chad@chad-laptop.(none)>
2014-01-19 05:40:39 -0800

    Spikes can be bloodied.

--- src/main/Spike.java
+++ src/main/Spike.java
@@ -2,15 +2,25 @@ package main;
 
 public class Spike extends Platform {
 
-	public static TexturedRect texture =
+	public static TexturedRect textureClean =
 			new TexturedRect("assets/textures/spike.png", false, true);
+	public static TexturedRect textureBloody =
+			new TexturedRect("assets/textures/spikeDirty.png", false, true);
+	
+	private boolean bloody;
 	
 	public Spike(Vec2 pos, Vec2 dim) {
 		super(pos, dim);
+		bloody = false;
+	}
+	
+	public void bloody() {
+		bloody = true;
 	}
 
 	@Override
 	public void draw() {
+		TexturedRect texture = bloody ? textureBloody : textureClean;
 		texture.setPos(getPos());
 		texture.setDim(getDim());
 		texture.drawWrapped();
